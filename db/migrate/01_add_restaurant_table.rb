6a0a6176fc0a91ea7b4b646ebefa5de0ada3f2e8
Matt Steele <orphum@gmail.com>
2012-04-14 15:43:47 -0500

    add data importer to populate sqlite table

--- db/migrate/01_add_restaurant_table.rb
+++ db/migrate/01_add_restaurant_table.rb
@@ -6,7 +6,7 @@ class AddRestaurantTable < ActiveRecord::Migration
       t.string :rating
       t.datetime :inspection_date
       t.decimal :latitude, :precision => 3, :scale => 20
-      t.decimal :longitude, :precision => 3, :sclae => 20
+      t.decimal :longitude, :precision => 3, :scale => 20
       t.string :photo
       t.string :tip
     end
