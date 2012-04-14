a9838fb1327a9e70befaca993666cabcbb55ea82
Steven Samson <kosai03@gmail.com>
2012-04-14 18:14:17 -0500

    better SOC, updated API docs

--- models/restaurant.rb
+++ models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ActiveRecord::Base
+  
+  def self.get_opponent
+    max_range = Restaurant.count
+    row = Random.rand(max_range) + 1
+    
+    Restaurant.first(:offset => row)
+  end
 end
